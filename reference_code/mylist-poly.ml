197a22f95ab4c9fd985a169da21a05254675f015
Abhinav Kumar <Abhinav.Kumar@iiitb.ac.in>
2026-02-10 23:26:27 +0530

    fomat all ocaml code

--- reference_code/mylist-poly.ml
+++ reference_code/mylist-poly.ml
@@ -1,21 +1,16 @@
-type 'a mylist =
-        Empty
-  | NonEmpty of 'a * 'a mylist
+type 'a mylist = Empty | NonEmpty of 'a * 'a mylist
 
-let rec sumlist = function
-   Empty -> 0
- | NonEmpty(h, t) -> h + (sumlist t)
+let rec sumlist = function Empty -> 0 | NonEmpty (h, t) -> h + sumlist t
+let rec length = function Empty -> 0 | NonEmpty (h, t) -> 1 + length t
+let l1 = NonEmpty (1, NonEmpty (2, Empty))
+let l2 = NonEmpty ("one", NonEmpty ("two", Empty))
 
-let rec length = function
-   Empty -> 0
- | NonEmpty(h, t) -> 1 + (length t)
-
-let l1 = NonEmpty(1, NonEmpty(2, Empty))
-
-let l2 = NonEmpty("one", NonEmpty("two", Empty))
 let main () =
-        let s = (sumlist l1) in (print_endline (string_of_int s));
-        let n = (length l1) in (print_endline (string_of_int n));
-        let n2 = (length l2) in (print_endline (string_of_int n2))
+  let s = sumlist l1 in
+  print_endline (string_of_int s);
+  let n = length l1 in
+  print_endline (string_of_int n);
+  let n2 = length l2 in
+  print_endline (string_of_int n2)
 
 let _ = main ()
--- reference_code/mylist.ml
+++ reference_code/mylist.ml
@@ -1,19 +1,13 @@
-type mylist =
-        Empty
-  | NonEmpty of int * mylist
+type mylist = Empty | NonEmpty of int * mylist
 
-let rec sumlist = function
-   Empty -> 0
- | NonEmpty(h, t) -> h + (sumlist t)
-
-let rec length = function
-   Empty -> 0
- | NonEmpty(h, t) -> 1 + (length t)
-
-let l1 = NonEmpty(1, NonEmpty(2, Empty))
+let rec sumlist = function Empty -> 0 | NonEmpty (h, t) -> h + sumlist t
+let rec length = function Empty -> 0 | NonEmpty (h, t) -> 1 + length t
+let l1 = NonEmpty (1, NonEmpty (2, Empty))
 
 let main () =
-        let s = (sumlist l1) in (print_endline (string_of_int s));
-        let n = (length l1) in (print_endline (string_of_int n))
+  let s = sumlist l1 in
+  print_endline (string_of_int s);
+  let n = length l1 in
+  print_endline (string_of_int n)
 
 let _ = main ()
