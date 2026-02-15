31f401d07712435d04ab03ffe17a901e62fcceb7
Abhinav Kumar <Abhinav.Kumar@iiitb.ac.in>
2026-02-15 11:07:24 +0530

    refined derivative.ml in assignment-6

--- assignment-6/derivative.ml
+++ assignment-6/derivative.ml
@@ -1,7 +1,5 @@
-let derivative f =
-  let h = 0.00001 in
-  fun x -> (f (x +. h) -. f x) /. h
-
+let raw_derivative h f = fun x -> (f (x +. h) -. f x) /. h
+let derivative = raw_derivative 0.00001
 let dbl_derivative f = derivative (derivative f)
 let id x = x ** 3.
 let main () = print_endline (string_of_float ((dbl_derivative id) 3.))
