daddd5529b446329d107fe4912c652edb5f3fe70
Abhinav Kumar <Abhinav.Kumar@iiitb.ac.in>
2026-02-15 23:39:58 +0530

    imporoved tail recursive fibonacci

--- misc/fib_tail.ml
+++ misc/fib_tail.ml
@@ -2,13 +2,13 @@ let fib_tail n =
   if n <= 0 then 0
   else if n = 1 then 1
   else
-    let rec aux a b c i = if i = 0 then c else aux b c (b + c) (i - 1) in
-    aux 0 1 1 (n - 2)
+    let rec aux b c i = if i = 0 then c else aux c (b + c) (i - 1) in
+    aux 1 1 (n - 2)
 
 let main () =
   begin
-    print_int (fib_tail 3);
-    print_newline ();
+    print_int (fib_tail 8);
+    print_newline ()
   end
 
 let _ = main ()
