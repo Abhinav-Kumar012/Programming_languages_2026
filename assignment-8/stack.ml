bf63db317750bf4cf9e3a6205a65930a18d7faa5
Abhinav Kumar <Abhinav.Kumar@iiitb.ac.in>
2026-02-19 10:04:44 +0530

    assignment - 8 in progress

---
+++ assignment-8/stack.ml
@@ -0,0 +1,29 @@
+module type STACK = sig
+  type 'a t (* abstract type *)
+
+  val empty : 'a t
+  val push : 'a -> 'a t -> 'a t
+  val pop : 'a t -> 'a t
+  val top : 'a t -> 'a
+  val is_empty : 'a t -> bool
+end
+
+module Stack : STACK = struct
+  type 'a t = 'a list
+
+  let empty = []
+  let push e s = e :: s
+  let pop s = match s with [] -> failwith "empty stack" | _ :: t -> t
+  let top s = match s with [] -> failwith "empty stack" | h :: _ -> h
+  let is_empty = List.is_empty
+end
+
+let main () =
+  let st =
+     Stack.empty in
+  let st1 = Stack.push 4 st in
+  print_endline (string_of_int (Stack.top st1));
+  let st2 = Stack.pop st1 in
+  print_endline (string_of_bool (Stack.is_empty st2))
+
+let _ = main ()
