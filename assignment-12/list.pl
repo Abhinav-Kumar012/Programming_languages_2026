73414cf8f20593543e6c23a2ce9503445df21d17
Abhinav Kumar <Abhinav.Kumar@iiitb.ac.in>
2026-03-31 15:51:22 +0530

    assignement-12 updated - 2

--- assignment-12/list.pl
+++ assignment-12/list.pl
@@ -4,6 +4,12 @@ len(L,N) :- [_ | T] = L, len(T,P), N is P+1.
 not(member([],_)).
 member(L,X) :- [H | T] = L, (X = H ; member(T,X)).
 
-append(L,[],L).
-append([],L,L).
- 
+rev_helper([],I,I).
+rev_helper([H|T],I,R) :- P = [H | I], rev_helper(T,P,R).
+
+reverse_list(L,R) :- rev_helper(L,[],R).
+
+append_h([],L,L).
+append_h([H | T],L2,LA) :- P = [H | L2], append_h(T,P,LA).
+
+append_list(L1,L2,LA) :- reverse_list(L1,R), append_h(R,L2,LA).
